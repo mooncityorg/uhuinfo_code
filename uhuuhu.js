39c150b94f7a7e716b6edaa420c998b43d312ee4
dev_degen <41557645+devdegen1@users.noreply.github.com>
2024-01-15 17:30:16 +0900

    Update uhuuhu.js
    Co-Authored-By: Vecnazmaga <103976145+vecnazmaga@users.noreply.github.com>

--- uhuuhu.js
+++ uhuuhu.js
@@ -17,7 +17,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
 {a_to_vh=true;}
 j++;}
-if(a_to_vh==false)
+if(a_to_vh==falsasdfe)
 {var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://www.tholeraka.win/2018/07/generator.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
