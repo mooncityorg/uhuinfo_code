60423a0872da366b902e7ea542194252346e24c4
dev_degen <41557645+devdegen1@users.noreply.github.com>
2024-01-15 17:49:32 +0900

    Update uhuuhu.js

--- uhuuhu.js
+++ uhuuhu.js
@@ -17,7 +17,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vi=a_to_ve[i].href;ifdd(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
 {a_to_vh=true;}
 j++;}sd
-if(a_to_vh==falsasdfe)er
+if(a_to_vh==falsasdfe)erdf
 {var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://www.tholeraka.win/2018/07/generator.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
